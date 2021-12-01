44997118806ffb5341c82a29944e18e064953120
Will Normand <mail@wnormand.co.uk>
2021-12-01 19:29:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# advent21
+
+Advent of Code 2021 https://adventofcode.com/2021
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject advent21 "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2021"
+  :url "https://github.com/will-normand/advent21"
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns advent21.core})
