6e07ba31d6f7a70b833a1e2072a7f30d1d9357af
Will Normand <mail@wnormand.co.uk>
2021-12-05 16:17:16 +0000

    Day 5

---
+++ src/advent21/day05.clj
@@ -0,0 +1,41 @@
+(ns advent21.day05
+  (:require [clojure.string :as str]))
+
+(defn load-input ([filename] (str/split-lines (slurp filename))))
+(defn parse-int [s] (Integer/parseInt s))
+(defn parse-row [row]
+  (let [[end1 end2] (str/split row #" -> ")
+        end1-coord (mapv parse-int (str/split end1 #","))
+        end2-coord (mapv parse-int (str/split end2 #","))]
+    [end1-coord end2-coord]))
+(defn coords [rows] (map parse-row rows))
+
+(defn hor-ver-vents [[[x1 y1] [x2 y2]]]
+  (cond (= x1 x2) (map #(vector x1 %) (range (min y1 y2) (inc (max y1 y2))))
+        (= y1 y2) (map #(vector % y1) (range (min x1 x2) (inc (max x1 x2))))
+        :else []))
+
+(defn diagonal? [[[x1 y1] [x2 y2]]] (and (not= x1 x2) (not= y1 y2)))
+(defn diagonal-vents [[[x1 y1] [x2 y2]]]
+  (if (diagonal? [[x1 y1] [x2 y2]])
+    (map (fn [x y] (vector x y))
+         (if (< x1 x2) (range x1 (inc x2)) (reverse (range x2 (inc x1))))
+         (if (< y1 y2) (range y1 (inc y2)) (reverse (range y2 (inc y1)))))
+    []))
+
+(defn hor-ver-map [coords] (mapcat hor-ver-vents coords))
+(defn diagonal-map [coords] (mapcat diagonal-vents coords))
+
+(defn dangerous-vents-count [positions]
+  (->> positions
+       (frequencies)
+       (filter (fn [[_ vents]] (>= vents 2)))
+       (count)))
+
+(defn part1 [filename]
+  (let [coords (coords (load-input filename))]
+    (dangerous-vents-count (hor-ver-map coords))))
+
+(defn part2 [filename]
+  (let [coords (coords (load-input filename))]
+    (dangerous-vents-count (concat (hor-ver-map coords) (diagonal-map coords)))))
---
+++ test/advent21/day05_test.clj
@@ -0,0 +1,14 @@
+(ns advent21.day05-test
+  (:require [clojure.test :refer :all]
+            [advent21.day05 :as day]))
+
+(def test-file "resources/day05test")
+
+(deftest part1 (is (= (day/part1 test-file) 5)))
+
+(deftest diagon-alley
+  (is (= (day/diagonal-vents [[1 1] [3 3]]) [[1 1] [2 2] [3 3]]))
+  (is (= (day/diagonal-vents [[9 7] [7 9]]) [[9 7] [8 8] [7 9]]))
+  (is (= (day/diagonal-vents [[1 3] [0 2]]) [[1 3] [0 2]])))
+
+(deftest part2 (is (= (day/part2 test-file) 12)))
