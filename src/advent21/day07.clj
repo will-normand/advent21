05b6e5b69c74e4d65f68ea3944eb0c5d097cd5a8
Will Normand <mail@wnormand.co.uk>
2021-12-07 20:15:29 +0000

    Day 7, memoize fuel calculation
    Reduces time taken by about a factor of 10.

--- src/advent21/day07.clj
+++ src/advent21/day07.clj
@@ -2,8 +2,9 @@
   (:require [advent21.utils.input :as i]))
 
 (defn fuel [distance] (reduce + (range 1 (inc distance))))
+(def fuel-memo (memoize fuel))
 (defn basic-fuel-to-move [x crabs] (reduce + (map (fn [crab] (Math/abs ^Integer (- x crab))) crabs)))
-(defn more-fuel-to-move [x crabs] (reduce + (map (fn [crab] (fuel (Math/abs ^Integer (- x crab)))) crabs)))
+(defn more-fuel-to-move [x crabs] (reduce + (map (fn [crab] (fuel-memo (Math/abs ^Integer (- x crab)))) crabs)))
 (defn min-fuel [crabs fuel-fn] (apply min (map #(fuel-fn % crabs) (range (apply min crabs) (inc (apply max crabs))))))
 
 (defn part1 [filename] (min-fuel (i/parse-int-row filename) basic-fuel-to-move))
