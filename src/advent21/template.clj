e9f3aa5536d5c6e951a00ccf9ce67c4b3b01f6f1
Will Normand <mail@wnormand.co.uk>
2021-12-11 12:41:07 +0000

    Update template

--- src/advent21/template.clj
+++ src/advent21/template.clj
@@ -1,7 +1,6 @@
 (ns advent21.template
-  (:require [clojure.string :as str]))
-
-(defn load-input ([filename] (str/split-lines (slurp filename))))
+  (:require [clojure.string :as str]
+            [advent21.utils.input :as i]))
 
 (defn part1 [filename])
 
--- test/advent21/template_test.clj
+++ test/advent21/template_test.clj
@@ -2,8 +2,7 @@
   (:require [clojure.test :refer :all]
             [advent21.template :as day]))
 
-(def test-file "resources/dayXXtest")
+(def test-file "dayXXtest")
 
-(deftest part1 (is (= (day/part1 test-file) nil))
-               )
+(deftest part1 (is (= (day/part1 test-file) nil)))
 (deftest part2 (is (= (day/part2 test-file) nil)))
