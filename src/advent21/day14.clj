fbaaa0660ca13378e525a39b997f51dc92dfe23f
Will Normand <mail@wnormand.co.uk>
2021-12-16 06:59:36 +0000

    Day 14, part 2

--- src/advent21/day14.clj
+++ src/advent21/day14.clj
@@ -1,5 +1,6 @@
 (ns advent21.day14
   (:require [clojure.string :as str]
+            [clojure.test :as t]
             [advent21.utils.input :as i]))
 
 (defn parse-input [filename]
@@ -13,26 +14,66 @@
 
 (defn step [polymer rules]
   (loop [[a b & rest] polymer
-         new-polymer []]
+         new-polymer '()]
     (if (nil? b)
-      (conj new-polymer a)
+      (reverse (conj new-polymer a))
       (if-let [c (get rules [a b])]
-        (recur (conj rest b) (conj new-polymer a c))
-        (recur (conj rest b) (conj new-polymer a))))))
+        (recur
+          (conj rest b)
+          (conj new-polymer a c))
+        (recur
+          (conj rest b)
+          (conj new-polymer a))))))
 
-(defn most-least-common [polymer] (let [freq (frequencies polymer)
-                                        sorted-freqs (sort-by val freq)]
-                                    [(second (last sorted-freqs)) (second (first sorted-freqs))]))
+(defn pairwise-step
+  {:test (fn [] (do (t/is (= {}
+                             (pairwise-step ['x 'y] {} 10)))
+                    (t/is (= {'z 1}
+                             (pairwise-step ['x 'y] {['x 'y] 'z} 1)))
+                    (t/is (= {'z 1}
+                             (pairwise-step ['x 'y] {['x 'y] 'z} 10)))))}
+  [pair rules steps]
+  (let [pairwise-step0
+        (memoize
+          (fn [pairwise-step0 pair step]
+            (let [[a b] pair]
+              (if (= step steps)
+                {}
+                (if-let [c (get rules [a b])]
+                  (merge-with +
+                              {c 1}
+                              (pairwise-step0 pairwise-step0 [a c] (inc step))
+                              (pairwise-step0 pairwise-step0 [c b] (inc step)))
+                  {})))))
+        pairwise-step0 (partial pairwise-step0 pairwise-step0)]
+    (pairwise-step0 pair 0)))
+
+(defn pairwise-steps [polymer rules steps]
+  (let [pairs (partition 2 1 polymer)
+        polymer-freqs (frequencies polymer)]
+    (->> (conj (map #(pairwise-step % rules steps) pairs) polymer-freqs)
+         (reduce (partial merge-with +)))))
+
+(defn most-least-common [freqs]
+  (let [sorted-freqs (sort-by val freqs)]
+    [(second (last sorted-freqs)) (second (first sorted-freqs))]))
 
 (defn do-steps [polymer rules steps]
-  (reduce (fn [polymer _] (time (step polymer rules))) polymer (repeat steps 1)))
+  (reduce (fn [polymer _] #_(time) (step polymer rules)) polymer (repeat steps 1)))
 
 (defn calc-diff [filename steps]
   (let [input (parse-input filename)
         polymer (do-steps (:template input) (:rules input) steps)
-        [most least] (most-least-common polymer)]
+        [most least] (most-least-common (frequencies polymer))]
+    (- most least)))
+
+(defn calc-diff-depth [filename steps]
+  (let [input (parse-input filename)
+        freqs (pairwise-steps (:template input) (:rules input) steps)
+        [most least] (most-least-common freqs)]
     (- most least)))
 
 (defn part1 [filename] (calc-diff filename 10))
+(defn part1-depth [filename] (calc-diff-depth filename 10))
 
-(defn part2 [filename])
+(defn part2 [filename] (calc-diff-depth filename 40))
